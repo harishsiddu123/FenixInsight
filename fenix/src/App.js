9109e255e97e13a7dfb19f3c1274322d15991d80
harishsiddu123 <114544717+harishsiddu123@users.noreply.github.com>
2023-05-28 17:44:40 +0530

    Add files via upload

---
+++ fenix/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import FenixWebsite from './components/FenixWebsite';
+
+function App() {
+  return (
+    <div className="App">
+      <FenixWebsite></FenixWebsite> 
+    </div>
+  );
+}
+
+export default App;
